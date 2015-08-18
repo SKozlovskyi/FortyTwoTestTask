4d23e3212371b6456975a4061587a78883000126
Sergii Kozlovskyi <sergiikozlovskyi@gmail.com>
2015-08-18 17:52:08 +0300

    modell_settigs_migration

--- apps/hello/models.py
+++ apps/hello/models.py
@@ -1,3 +1,13 @@
+# -*- encoding: utf-8 -*-
 from django.db import models
 
-# Create your models here.
+
+class Person(models.Model):
+    first_name = models.CharField("First Name", max_length=30)
+    last_name = models.CharField("Last Name", max_length=30)
+    birth_date = models.DateField("Date of Birth")
+    con_email = models.EmailField("Email", max_length=79)
+    con_jabbber = models.EmailField("Jabber", max_length=79)
+    con_skype = models.CharField("Skype", max_length=79)
+    bio = models.TextField("Bio", max_length=400)
+    con_other = models.TextField("Other Contacts", max_length=79, blank=True)
